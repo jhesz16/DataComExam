333583e40470eb9e6e49362562e9781ee77b5874
jhesz16 <86293479+jhesz16@users.noreply.github.com>
2022-01-21 10:35:01 +0800

    Add files via upload

---
+++ src/test/java/Driver/Setup.java
@@ -0,0 +1,84 @@
+package Driver;
+
+import TestData.Data;
+import io.github.bonigarcia.wdm.OperatingSystem;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManagerException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+
+public class Setup {
+
+    public static WebDriver driver;
+    private static Properties prop;
+
+    public static void getDriver(String Browser) throws IOException {
+        switch (Browser.toUpperCase(Locale.ROOT)) {
+            case "CHROME": {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.setBinary("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
+                driver = new ChromeDriver(options);
+                break;
+            }
+            case "BRAVE": {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.setBinary("C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe");
+                driver = new ChromeDriver(options);
+                break;
+            }
+            case "EDGE": {
+                WebDriverManager.edgedriver().operatingSystem(OperatingSystem.WIN).setup();
+                driver = new EdgeDriver();
+                break;
+            }
+            case "FIREFOX": {
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions options = new FirefoxOptions();
+                options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
+                driver = new FirefoxDriver(options);
+                break;
+            }
+            default:
+                throw new WebDriverManagerException(("Browser "+Browser+" not found"));
+
+        }
+        driver.get(getUrl());
+    }
+
+    public static String getProperty(String property) throws IOException {
+        FileInputStream fis = null;
+        prop = null;
+        try {
+            fis = new FileInputStream("target/classes/config.properties");
+            prop = new Properties();
+            prop.load(fis);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            fis.close();
+        }
+        return prop.getProperty(property);
+    }
+
+    public static String getBrowser() throws IOException {
+        return getProperty("browserConfig");
+    }
+    public static String getUrl() throws IOException {
+        return getProperty("URL");
+    }
+}
---
+++ src/test/java/PayeeTest.java
@@ -0,0 +1,85 @@
+import Helpers.GlobalHelpers;
+import Pages.Home;
+import Pages.Payee;
+import Pages.Payments;
+import jdk.jfr.Description;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static Driver.Setup.*;
+
+public class PayeeTest {
+    public static GlobalHelpers helpers;
+    public static Home home;
+    public static Payee payee;
+    public static Payments payments;
+
+    @BeforeMethod
+    public void setupClass() throws IOException {
+        getDriver(getBrowser());
+    }
+
+    @Description("TC1: Verify you can navigate to Payees page using the navigation menu")
+    @Test(description = "TC1")
+    public void TC1(){
+        home = new Home();
+        payee = new Payee();
+        payments = new Payments();
+        helpers = new GlobalHelpers();
+        home.navigateTo("Payees");
+        payee.verifyPayeesPage();
+    }
+
+    @Description("TC2: Verify you can add new payee in the Payees page")
+    @Test(description = "TC2")
+    public void TC2() {
+        home = new Home();
+        payee = new Payee();
+        payments = new Payments();
+        helpers = new GlobalHelpers();
+        String payeeName = "TYCO CYLINDER TESTING";
+        home.navigateTo("Payees");
+        payee.addPayee(payeeName);
+        payee.verifyPayeeInTable(payeeName);
+    }
+
+    @Description("Verify payee name is a required field")
+    @Test
+    public void TC3() {
+        home.navigateTo("Payees");
+        payee.validateAddBlankPayee();
+    }
+
+    @Description("TC4: Verify that payees can be sorted by name")
+    @Test(description = "TC3")
+    public void TC4() {
+        String payeeName = "TYCO CYLINDER TESTING";
+        home.navigateTo("Payees");
+        List<String> payeesNames = payee.getPayeesNames();
+        List<String> ascending = new ArrayList<>(payeesNames);
+        List<String> descending = new ArrayList<>(payeesNames);
+        Collections.sort(ascending);
+        Collections.reverse(descending);
+        Assert.assertEquals(payeesNames, ascending);
+
+        payee.clickNameHeader();
+        Assert.assertEquals(payee.getPayeesNames(), descending);
+    }
+
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+}
+
+
